a6e0626c8c8c0174758fe9fdcf5b151a503624b3
Nodir Kosimkhujaev <128922164+zSayid@users.noreply.github.com>
2024-04-16 05:16:04 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,18 +1,28 @@
-document
-  .getElementById("search-form")
-  .addEventListener("submit", async (event) => {
+const loader = document.querySelector(".loader");
+
+document.getElementById("search-form").addEventListener("submit", async (event) => {
     event.preventDefault();
 
     const searchInput = document.querySelector('input[name="term"]');
     const searchResults = document.getElementById("search-results");
-    const query = searchInput.value.trim();
+    const query = searchInput.value.trim();  
 
     if (query === "") {
       return;
     }
 
+    function hideResult(){
+      searchResults.style.display = "none"; // Hide search results container
+     
+    }
+    function showResult() {
+      searchResults.style.display = "flex"; // Show search results container
+      searchResults.style.flexWrap = "nowrap"; // Use camelCase for flexWrap
+      searchResults.style.justifyContent = "space-around"; // Use camelCase for justifyContent
+      searchResults.style.gap = "6px"; // No change needed here
+    }
+      
     searchResults.innerHTML = ""; // Clear previous search results
-
     const repos = [
       {
         title: "arXiv",
@@ -26,16 +36,22 @@ document
       {
         title: "NCBI PMC",
       },
-      {
-        title: "PDF Search",
-      },
+    // {
+    //   title: "PDF Search",
+    // },
     ];
 
+    
     for (const repo of repos) {
       const repoDiv = document.createElement("div");
       repoDiv.className = "repository";
       repoDiv.innerHTML = `<h3>${repo.title}</h3>`;
       searchResults.appendChild(repoDiv);
+
+
+      hideResult();
+      loader.style.display = "block"
+      // Fetch search results from the server
 
       try {
         const response = await fetch(`/search?q=${query}`);
@@ -43,12 +59,13 @@ document
         const parser = new DOMParser();
         const doc = parser.parseFromString(htmlText, "text/html");
 
-        //======================================================================================= Arxiv Search================================================================
+        //======================================================================================= Arxiv Search================================================================        
 
         if (repo.title === "arXiv") {
           try {
             const response = await fetch(`/arxiv?q=${query}`);
             const jsonData = await response.json();
+
 
             if (jsonData.length === 0) {
               repoDiv.innerHTML += "<p>No results found.</p>";
@@ -86,11 +103,18 @@ document
                 updatedEl.innerHTML =
                   "<strong>Updated Date:</strong>: " + paperData.updated;
 
+                // extract DOI from the paper
+                const doi = document.createElement('p');
+                doi.className = "doi";
+                doi.innerHTML = "<strong>doi:</strong> " + paperData.id;
+
+                
                 paperDiv.appendChild(titleLink);
                 paperDiv.appendChild(authorEl);
                 paperDiv.appendChild(summaryElement);
                 paperDiv.appendChild(publishedData);
                 paperDiv.appendChild(updatedEl);
+                paperDiv.appendChild(doi);
 
                 repoDiv.appendChild(paperDiv); // Append the paper div to the repository div
               }
@@ -160,7 +184,7 @@ document
             <p><strong>Author(s): </strong> ${authors.join(", ")}</p>
             <p><strong>PMCID:</strong> ${pmcId}</p>
             <p><strong>Date:</strong> ${pmcDate}</p>
-            <p><strong>Doi: </strong>${doi}</p>
+            <p><strong>${doi}</strong></p>
             <p class="article">${articleLinks.join(" ")}</p>
           `;
 
@@ -196,6 +220,12 @@ document
                 paperItem.querySelectorAll(".highwire-cite-metadata-doi")
               ).map((doi) => doi.textContent);
 
+              
+      
+              const dateString = bioDate[0];
+              const dateArray = dateString.match(/\d{4}\.\d{2}\.\d{2}/);
+              const formattedDate = dateArray ? dateArray[0] : "Date not found";
+
               // directing to the desired page
               const href = paperItem.querySelector("a").getAttribute("href");
               const titleLink = document.createElement("a");
@@ -212,9 +242,9 @@ document
               }
               paperDiv.innerHTML += `
             <p><strong>Author(s): </strong> ${authors.join(", ")}</p>
+            <p><strong>Date:</strong> ${formattedDate}</p>
             <p> <strong>Journal: </strong> ${journal}</p>
-            <p> <strong>Date: </strong> ${bioDate}</p>
-            <p> <strong>Doi: </strong> ${doi}</p>
+            <p> <strong>${doi}</strong></p>
             `;
               repoDiv.appendChild(paperDiv);
             });
@@ -241,12 +271,16 @@ document
               const journal = Array.from(
                 paperItem.querySelectorAll(".highwire-cite-metadata-journal")
               ).map((journal) => journal.textContent);
-              const bioDate = Array.from(
+              const medDate = Array.from(
                 paperItem.querySelectorAll(".highwire-cite-metadata-pages")
-              ).map((bioDate) => bioDate.textContent);
+              ).map((medDate) => medDate.textContent);
               const doi = Array.from(
                 paperItem.querySelectorAll(".highwire-cite-metadata-doi")
               ).map((doi) => doi.textContent);
+
+              const dateString = medDate[0];
+              const dateArray = dateString.match(/\d{4}\.\d{2}\.\d{2}/);
+              const formattedDate = dateArray ? dateArray[0] : "Date not found";
 
               // directing to the desired page
               const href = paperItem.querySelector("a").getAttribute("href");
@@ -265,61 +299,17 @@ document
 
               paperDiv.innerHTML += `
             <p><strong>Author(s): </strong> ${authors.join(", ")}</p>
+            <p><strong>Date:</strong> ${formattedDate}</p>
             <p> <strong>Journal: </strong> ${journal}</p>
-            <p> <strong>Date: </strong> ${bioDate}</p>
-            <p> <strong>Doi: </strong> ${doi}</p>
+            <p> <strong>${doi}</strong></p>
             `;
               repoDiv.appendChild(paperDiv);
             });
           }
         }
+        showResult();
+        loader.style.display = "none";
 
-        //=============================================================================== pdf search ==================================================================
-        // ...
-        if (repo.title === "PDF Search") {
-          try {
-            const fetchingData = await fetch(`/data?q=${query}`);
-            const responses = await fetchingData.json();
-
-            if (responses.length === 0) {
-              repoDiv.innerHTML += "<p>No results found</p>";
-            } else {
-              for (const response of responses) {
-                const paperDiv = document.createElement("div");
-                paperDiv.className = "paper";
-
-                const title = response.title;
-                const snippet = response.snippet;
-                const authors = response["publication_info"].summary;
-                const downloadLink = document.createElement("a");
-                downloadLink.textContent = "Download PDF"; // Text for the link
-                downloadLink.href = response.link;
-
-                // Create elements to display the data ====================
-
-                const titleElement = document.createElement("h4");
-                titleElement.textContent = title;
-
-                const sumEl = document.createElement("p");
-                sumEl.textContent = snippet;
-
-                const authorsElement = document.createElement("p");
-                authorsElement.innerHTML = "<p><strong>Author(s): </strong></p> " + authors
-               
-                // Append the elements to the paperDiv
-                paperDiv.appendChild(titleElement);
-                paperDiv.appendChild(sumEl);
-                paperDiv.appendChild(authorsElement);
-                paperDiv.appendChild(downloadLink);
-
-                // Append the paperDiv to the results container
-                repoDiv.appendChild(paperDiv);
-              }
-            }
-          } catch (error) {
-            console.log(error);
-          }
-        }
       } catch (error) {
         console.error(`Error fetching data from ${repo.title}:`, error);
         repoDiv.innerHTML +=
